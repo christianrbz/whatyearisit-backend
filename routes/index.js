76794e988df8b097de74c56cacd2ea519102ef0b
Christian Reubrez <christian.reubrez@gmail.com>
2023-03-03 10:58:55 +0100

    second commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// GET year 
+router.get('/year', function(req, res, next) {
+  res.json({ year: new Date().getFullYear()});
+});
+
+
+
 module.exports = router;
